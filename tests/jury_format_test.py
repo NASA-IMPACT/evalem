93b66d66cc9b7a67f289394e3bccc345a6a805ba
NISH1001 <nishanpantha@gmail.com>
2023-02-21 15:30:40 -0600

    Add more tests for jury format conversion

--- tests/jury_format_test.py
+++ tests/jury_format_test.py
@@ -16,13 +16,34 @@ class References:
         DTOS = [ReferenceDTO(text="Reference 1"), ReferenceDTO(text="Reference 2")]
 
         MIXED = [
-            "Reference 1",
-            dict(text="Reference 2"),
-            ReferenceDTO(text="Reference 3"),
+            dict(text="Reference 1"),
+            ReferenceDTO(text="Reference 2"),
+        ]
+
+    class Multi:
+        STRS = [
+            ["Reference 1.1", "Reference 1.2"],
+            ["Reference 2.1", "Reference 2.2"],
+        ]
+
+        DICTS = [
+            [dict(text="Reference 1.1"), dict(text="Reference 1.2")],
+            [dict(text="Reference 2.1"), dict(text="Reference 2.2")],
+        ]
+
+        DTOS = [
+            [ReferenceDTO(text="Reference 1.1"), ReferenceDTO(text="Reference 1.2")],
+            [ReferenceDTO(text="Reference 2.1"), ReferenceDTO(text="Reference 2.2")],
+        ]
+
+        MIXED = [
+            ["Reference 1.1", ReferenceDTO(text="Reference 1.2")],
+            [dict(text="Reference 2.1"), ReferenceDTO(text="Reference 2.2")],
         ]
 
 
 def test_single_strs():
+    # Identity
     assert format_to_jury(References.Single.STRS) == References.Single.STRS
 
 
@@ -35,8 +56,27 @@ def test_single_dtos():
 
 
 def test_single_mixed():
-    assert format_to_jury(References.Single.MIXED) == [
-        "Reference 1",
-        "Reference 2",
-        "Reference 3",
-    ]
+    assert format_to_jury(References.Single.MIXED) == References.Single.STRS
+
+
+def test_multi_strs():
+    assert format_to_jury(References.Multi.STRS) == References.Multi.STRS
+
+
+def test_multi_dicts():
+    assert format_to_jury(References.Multi.DICTS) == References.Multi.STRS
+
+
+def test_multi_dots():
+    assert format_to_jury(References.Multi.DTOS) == References.Multi.STRS
+
+
+def test_multi_mixed():
+    assert format_to_jury(References.Multi.MIXED) == References.Multi.STRS
+
+
+def test_single_ints():
+    # right now, the jury conversion only works for only strings
+    refs = [1, 2, 3, 4]
+    assert format_to_jury(refs) == refs
+    assert format_to_jury(refs) != list(map(str, refs))
