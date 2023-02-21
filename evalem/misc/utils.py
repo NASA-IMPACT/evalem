d713f4d7872cdd0e7e05c79f32764573cbb6d615
NISH1001 <nishanpantha@gmail.com>
2023-02-21 12:35:36 -0600

    Bugfix jury format conversion function format_to_jury
    Now, it can handle arbritary list/dict/str/EvaluationDTO formats.

--- evalem/misc/utils.py
+++ evalem/misc/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import List, Union
+from typing import Iterable, List, Union
 
 from ..structures import EvaluationDTO, PredictionInstance, ReferenceInstance
 
@@ -10,22 +10,29 @@ def format_to_jury(
 ) -> Union[List[str], List[List[str]]]:
     if not instances:
         return []
+        # If single DTO object, wrap into a list
 
-    # If single DTO object, wrap into a list
-    if isinstance(instances, EvaluationDTO):
+    if isinstance(instances, (EvaluationDTO, dict, str)):
         return format_to_jury([instances])
 
-    # if single DTO as dict object, wrap a new DTO object into a list
-    elif isinstance(instances, dict):
-        return format_to_jury([EvaluationDTO.from_dict(instances)])
+    # converts everything to DTO
+    def _dtofy(instance):
+        if isinstance(instance, dict):
+            return EvaluationDTO.from_dict(instance)
+        if isinstance(instance, str):
+            return EvaluationDTO(text=instance)
+        return instance
 
-    elif isinstance(instances, list) and isinstance(instances[0], dict):
-        return format_to_jury(
-            list(map(lambda x: EvaluationDTO.from_dict(x), instances)),
-        )
-    elif isinstance(instances, list) and isinstance(instances[0], EvaluationDTO):
+    instances = (
+        list(map(_dtofy, instances)) if isinstance(instances, Iterable) else instances
+    )
+
+    # if not List[list] and only List[Type[EvaluationDTO]]
+    if isinstance(instances, list) and isinstance(instances[0], EvaluationDTO):
         return list(map(lambda x: x.text, instances))
+    # list of list handler
     elif isinstance(instances, list) and isinstance(instances[0], list):
+        instances = list(map(_dtofy, instances))
         return list(map(format_to_jury, instances))
     else:
         return instances
